e32be833fd8649ba958e8dac603d398b2e90b52a
Crasherqa <procrasher@gmail.com>
2017-11-06 16:49:05 +0200

    Lesson. Simple test 2

--- firstest/src/test/java/ua/org/autotest/FirsTest.java
+++ firstest/src/test/java/ua/org/autotest/FirsTest.java
@@ -1,9 +1,6 @@
 package ua.org.autotest;
 
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -20,7 +17,8 @@ public class FirsTest {
 
     @BeforeClass
     public static void setup() {
-        System.setProperty("webdriver.chrome.driver", "E://QA/Automation/File/chromedriver.exe");
+        //System.setProperty("webdriver.chrome.driver", "E://QA/Automation/File/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C://Automation Practic/chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -30,11 +28,26 @@ public class FirsTest {
     @Test
     public void userLogin() {
         WebElement loginField = driver.findElement(By.id("id-1"));
+
+        System.out.println(loginField.getRect());
+
         loginField.sendKeys("autotestorgua");
+        WebElement passwordField = driver.findElement(By.id("id-2"));
+        passwordField.sendKeys("testpass");
+        WebElement loginButton = driver.findElement(By.xpath("//div[text()='Увійти']"));
+        loginButton.click();
+        WebElement profileUser = driver.findElement(By.cssSelector(".login-button__user"));
+        String mailUser = profileUser.getText();
+        Assert.assertEquals("autotestorgua@ukr.net", mailUser);
+
     }
 
     @AfterClass
     public static void tearDown() {
+        WebElement menuUser = driver.findElement(By.cssSelector(".login-button__menu-icon"));
+        menuUser.click();
+        WebElement logoutButton = driver.findElement(By.id("login__logout"));
+        logoutButton.click();
         driver.close();
     }
 
